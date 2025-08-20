92cd04e3b008782a9a15eddf383084efb07295e9
Castro Matias <96086@sistemas.frc.utn.edu.ar>
2025-08-19 22:30:42 -0300

    Ayuda igna

--- TorneosAPI/Controllers/TorneosController.cs
+++ TorneosAPI/Controllers/TorneosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using TorneosAPI.Models;
@@ -6,6 +7,8 @@ using TorneosAPI.Adapters;
 using Microsoft.AspNetCore.Cors;
 using TorneosAPI.Logs;
 using Microsoft.AspNetCore.Http.HttpResults;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace TorneosAPI.Controllers
 {
--- TorneosAPI/Program.cs
+++ TorneosAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using TorneosAPI.Models;
 
@@ -6,18 +8,64 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TorneosDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+#region CorsRules 
+
+var CorsRules = "CorsRules";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsRules,
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+
+});
+#endregion
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddSwaggerGen(o =>
+{
+o.SwaggerDoc("v1",
+    new OpenApiInfo
+    {
+        Title = "Prueba API",
+        Description = "Una aplicacion simple para mostrar el funcionamiento de las APIs", 
+        Version = "v1",
+        TermsOfService = null,
+        Contact = new OpenApiContact
+        {
+            // Check for optional parameters 
+        },
+        License = new OpenApiLicense
+        {
+            // Optional Example 
+            // Name = "Proprietary", 
+            // Url = new Uri("https://someURLToLicenseInfo.com") 
+        }
+    });
+    o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+     $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline. 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service Manager API V1");
+    });
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
